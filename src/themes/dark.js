11d117abeec668f57a2f63b94cdea7e3e03f0c3a
Wallace027Dev <wallaceofc@hotmail.com>
2025-04-25 15:54:24 -0300

    alterando tema escuro (tema do portfolio)

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,13 +1,14 @@
 const darkTheme = {
   name: "dark",
   colors: {
-    primary: "#3d5cff",
-    secondary: "#e4e3e0",
-    tertiary: "#2F2F42",
+    primary: "#6A45D1",
+    secondary: "#aB8cff",
+    tertiary: "#E5E5FC",
 
-    textColor: "#fff",
+    textColor: "#FFF",
     bgColor: "#1F1F39",
-  },
+    gradient: `linear-gradient(125deg, #6a45d1 0%, #E5E5FC 100%)`
+  }
 };
 
 export default darkTheme;
